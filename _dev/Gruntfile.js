86cf2aebff7a04711e45c02953b6134fe2ab178b
Noel Noche <genkibit@users.noreply.github.com>
2017-02-25 13:45:31 -0500

    Add assets directory to copy task

--- _dev/Gruntfile.js
+++ _dev/Gruntfile.js
@@ -380,6 +380,7 @@ module.exports = function (grunt) {
           src: [
             '*.{ico,png,txt}',
             '*.html',
+            'assets/icons/*.{ico,png}',
             'images/{,*/}*.{webp}',
             'styles/fonts/{,*/}*.*'
           ]
